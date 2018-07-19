3d1997039121ae78b2235fe3398c10d105493519
Rahul <vermarahulkps@gmail.com>
2018-07-19 12:40:44 +0530

    adding a comment

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RNATranscriptionModule
 {
@@ -6,6 +7,18 @@ namespace RNATranscriptionModule
     {
         public static string ToRna(string nucleotide)
         {
+            Dictionary<char, string> comp = new Dictionary<char, string>();
+            comp.Add('G',"C");
+            comp.Add('C',"G");
+            comp.Add('T',"A");
+            comp.Add('A',"U");
+            string rna = "";
+
+            for (int i = 0; i < nucleotide.Length; i++) {
+                rna += comp[nucleotide[i]];
+            }
+            //added a comment;
+            return rna;
             throw new NotImplementedException("You need to implement this function.");
         }
     }
